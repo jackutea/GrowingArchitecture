4279628b4c42f62278c4a08f526095f2ec43897f
杰克有茶 <chenwansal1@163.com>
2023-07-13 15:48:00 +0800

    0. establish

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour {
+
+    void Start() {
+        Debug.Log("Hello World");
+    }
+
+    void Update() {
+
+    }
+
+}
